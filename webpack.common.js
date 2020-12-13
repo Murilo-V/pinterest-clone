829bdaf571dec1484c1ee0cbe699de3fa6be585c
Murilo-V <alveiramurilo@hotmail.com>
2020-12-13 18:07:17 -0300

    fix: build command errors

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ const path = require('path');
      }),
    ],
    output: {
-     filename: '[name].bundle.js',
+     filename: 'bundle.js',
      path: path.resolve(__dirname, 'dist'),
    },
  };
